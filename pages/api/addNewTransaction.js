e9b8ddb18707dabe400f32fa314d307c7af85e46
Brody Dingel <brodydingel795@gmail.com>
2020-05-24 10:53:00 -0500

    Refactor addNewTransaction code and db query functions

---
+++ pages/api/addNewTransaction.js
@@ -0,0 +1,171 @@
+const imaps = require("imap-simple");
+const db = require("./db");
+require("dotenv").config();
+// require("dotenv").config({ path: "../../.env" });
+
+let config = {
+	imap: {
+		user: process.env.EMAIL,
+		password: process.env.EMAIL_PASSWORD,
+		host: "imap.gmail.com",
+		port: 993,
+		tls: true,
+		authTimeout: 3000,
+	},
+};
+
+imaps.connect(config).then((connection) => {
+	return connection.openBox("INBOX").then(() => {
+		let searchCriteria = ["UNSEEN", ["FROM", process.env.BANK_EMAIL]];
+
+		let fetchOptions = {
+			bodies: ["HEADER", "TEXT"],
+			markSeen: false, // true in prod
+		};
+
+		return connection.search(searchCriteria, fetchOptions).then((emails) => {
+			if (emails.length > 0) processEmails(emails);
+			connection.imap.closeBox(true, (err) => {
+				if (err) {
+					console.log(err);
+				}
+			});
+			connection.end();
+		});
+	});
+});
+
+const processEmails = (emails) => {
+	db.getLastId().then((lastId) => {
+		let transactions = [];
+
+		const today = parseInt(
+			new Date().toISOString().slice(0, 10).replace(/-/g, "").replace(/$/, "00")
+		);
+
+		for (let i = 0; i < emails.length; i++) {
+			const body = emails[i].parts[0].body;
+			const id = today > lastId ? today + i + 1 : lastId + i + 1;
+
+			parseEmail(body)
+				.then((results) => {
+					transactions.push({
+						id,
+						...results,
+					});
+				})
+				.then(() => {
+					if (transactions.length === emails.length)
+						processTransactions(transactions);
+				})
+				.catch((error) => console.log(error));
+		}
+	});
+};
+
+const parseEmail = async (body) => {
+	let amount, debit, credit, comment;
+	const {
+		fastFoodLocations,
+		gasLocations,
+		groceriesLocations,
+		rentAmount,
+		carPaymentAmount,
+		salaryAmount,
+	} = await db.getTransactionIdentifiers();
+
+	if (body.includes("charged")) {
+		// transactions on credit card
+		amount_raw = /charged \$[\d,]+\.\d+ [^\.]+?\./.exec(body)[0];
+		amount = /[\d,]+\.\d+/.exec(amount_raw)[0];
+		amount = parseFloat(amount);
+
+		credit = "L_Credit_Card";
+
+		let location = /at [^\.]+?\./.exec(amount_raw)[0];
+		location = location.replace(/at |\./g, "");
+		comment = location;
+
+		// check if the location contains a substring of a location type (e.g., "McDonalds", "Hy-Vee")
+		if (
+			fastFoodLocations.some((fastFoodLocation) =>
+				location.includes(fastFoodLocation)
+			)
+		) {
+			debit = "E_Fast_Food";
+		} else if (
+			gasLocations.some((gasLocation) => location.includes(gasLocation))
+		) {
+			debit = "E_Gas";
+		} else if (
+			groceriesLocations.some((groceriesLocation) =>
+				location.includes(groceriesLocation)
+			)
+		) {
+			debit = "E_Groceries";
+		} else {
+			debit = "E_Other";
+		}
+	} else if (body.includes("Your transaction of")) {
+		// withdrawals from checking account
+		amount_raw = /transaction\s+of\s+\$[\d,]+\.\d+[^\.]+?\./.exec(body)[0];
+		amount = /[\d,]+\.\d+/.exec(amount_raw)[0];
+		amount = parseFloat(amount);
+
+		if (amount === parseFloat(rentAmount)) {
+			credit = "A_US_Bank";
+			debit = "E_Bills";
+			comment = "Rent";
+		} else if (amount === parseFloat(carPaymentAmount)) {
+			credit = "A_US_Bank";
+			debit = "E_Bills";
+			comment = "Car payment";
+		} else if (amount > 100) {
+			credit = "A_US_Bank";
+			debit = "L_Credit_Card";
+			comment = "Pay off credit card";
+		} else {
+			credit = "A_US_Bank";
+			debit = "E_Other";
+			comment = "Other";
+		}
+	} else if (body.includes("Deposit")) {
+		// 	deposits to checking account
+		amount_raw = /Deposit\s+of\s+\$[\d,]+\.\d+/.exec(body)[0];
+		amount = /[\d,]+\.\d+/.exec(amount_raw)[0];
+		amount = parseFloat(amount);
+		if (amount > parseFloat(salaryAmount)) {
+			credit = "I_Hy-Vee";
+			comment = "Salary from Hy-Vee";
+		} else {
+			credit = "I_Other";
+			comment = "Other income";
+		}
+		debit = "A_US_Bank";
+	}
+
+	return {
+		debit: debit,
+		credit: credit,
+		amount: amount,
+		comment: comment,
+	};
+};
+
+const processTransaction = async (transaction) => {
+	let accountBalances = await db.getLastAccountBalances(
+		transaction.debit,
+		transaction.credit,
+		transaction.id
+	);
+	console.log(accountBalances);
+	transaction.debit_balance = accountBalances.debit + transaction.amount;
+	transaction.credit_balance = accountBalances.credit - transaction.amount;
+	// await db.insertTransaction(transaction);
+};
+
+async function processTransactions(transactions) {
+	for (const transaction of transactions) {
+		await processTransaction(transaction);
+	}
+}

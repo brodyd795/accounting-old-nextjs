fea5a357f5a85d2b1d6c28445f2653134bd63cc0
Brody Dingel <brodydingel795@gmail.com>
2020-05-25 09:04:49 -0500

    Finish initial styling of summary table

--- components/tables/recent-table.js
+++ components/tables/recent-table.js
@@ -1,12 +1,33 @@
 import styled from "styled-components";
+import Link from "next/link";
 
 const StyledTable = styled.table`
-	border: 1px solid black;
+	border: 1px solid #333;
+	margin-bottom: 50px;
 
 	th,
 	td {
 		padding: 5px;
-		// text-align: center;
+		// border: 1px solid #333;
+	}
+
+	tr:nth-child(even) {
+		background-color: white;
+		color: #333;
+	}
+
+	.account-row {
+		cursor: pointer;
+	}
+
+	.account {
+		padding-left: 30px;
+		padding-right: 10px;
+	}
+
+	.balance {
+		padding-left: 30px;
+		padding-right: 30px;
 	}
 `;
 
@@ -18,20 +39,22 @@ const RecentTable = ({ data }) => (
 				<th>Balance</th>
 			</tr>
 			{Object.entries(data).map(([category, categoryValues], index) => (
-				<React.Fragment key={index}>
-					<tr>
-						<td>{category}</td>
-						<td>{categoryValues.balance}</td>
-					</tr>
-					{Object.entries(categoryValues.accounts).map(
-						([account, accountBalance], index) => (
-							<tr key={index}>
-								<td>{account}</td>
-								<td>{accountBalance}</td>
-							</tr>
-						)
-					)}
-				</React.Fragment>
+				<Link key={index} href="#">
+					<>
+						<tr>
+							<td>{category}</td>
+							<td className="balance">{categoryValues.balance}</td>
+						</tr>
+						{Object.entries(categoryValues.accounts).map(
+							([account, accountBalance], index) => (
+								<tr key={index} className="account-row">
+									<td className="account">{account}</td>
+									<td className="balance">{accountBalance}</td>
+								</tr>
+							)
+						)}
+					</>
+				</Link>
 			))}
 		</tbody>
 	</StyledTable>

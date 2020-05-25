d7717f3f660aa1a14eb2c7dc5b09e5a43270f380
Brody Dingel <brodydingel795@gmail.com>
2020-05-25 08:34:54 -0500

    Render summary data to UI

--- components/tables/recent-table.js
+++ components/tables/recent-table.js
@@ -6,7 +6,7 @@ const StyledTable = styled.table`
 	th,
 	td {
 		padding: 5px;
-		text-align: center;
+		// text-align: center;
 	}
 `;
 
@@ -17,19 +17,21 @@ const RecentTable = ({ data }) => (
 				<th>Account</th>
 				<th>Balance</th>
 			</tr>
-			{Object.entries(data).map(([category, categoryValue]) => (
-				<>
+			{Object.entries(data).map(([category, categoryValues], index) => (
+				<React.Fragment key={index}>
 					<tr>
 						<td>{category}</td>
-						<td>{categoryValue.balance}</td>
+						<td>{categoryValues.balance}</td>
 					</tr>
-					{Object.entries(categoryValue).map(([account, accountBalance]) => (
-						<tr>
-							<td>{account}</td>
-							{/* <td>{accountBalance}</td> */}
-						</tr>
-					))}
-				</>
+					{Object.entries(categoryValues.accounts).map(
+						([account, accountBalance], index) => (
+							<tr key={index}>
+								<td>{account}</td>
+								<td>{accountBalance}</td>
+							</tr>
+						)
+					)}
+				</React.Fragment>
 			))}
 		</tbody>
 	</StyledTable>

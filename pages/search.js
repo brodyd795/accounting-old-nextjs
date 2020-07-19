39fa649f70eb288c096ddd22a762551d5bc2e3e5
Brody Dingel <brodydingel795@gmail.com>
2020-07-19 16:33:16 -0500

    Get first amount input working

--- pages/search.js
+++ pages/search.js
@@ -35,6 +35,55 @@ const Search = () => {
 		}
 	};
 
+	const [amount, setAmount] = useState("");
+
+	const formatNumber = (n) => {
+		return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	};
+
+	const formatCurrency = (input, blur) => {
+		let input_val = input.target.value;
+
+		if (input_val === "") {
+			return;
+		}
+
+		if (input_val.indexOf(".") >= 0) {
+			let decimal_pos = input_val.indexOf(".");
+
+			let left_side = input_val.substring(0, decimal_pos);
+			let right_side = input_val.substring(decimal_pos);
+
+			left_side = formatNumber(left_side);
+			right_side = formatNumber(right_side);
+
+			if (blur) {
+				right_side += "00";
+			}
+
+			right_side = right_side.substring(0, 2);
+
+			input_val = "$" + left_side + "." + right_side;
+		} else {
+			input_val = formatNumber(input_val);
+			input_val = "$" + input_val;
+
+			if (blur) {
+				input_val += ".00";
+			}
+		}
+
+		setAmount(input_val);
+	};
+
+	const handleFromAmountChange = (e) => {
+		formatCurrency(e);
+	};
+
+	const handleFromAmountBlur = (e) => {
+		formatCurrency(e, true);
+	};
+
 	return (
 		<Page title={"Search"}>
 			<PageHeader text="Search" />
@@ -55,6 +104,18 @@ const Search = () => {
 						placeholder="Choose account..."
 						onChange={handleToAccountChange}
 						isClearable={true}
+					/>
+					<label>From:</label>
+					<input
+						type="text"
+						name="currency-field"
+						id="currency-field"
+						pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$"
+						value={amount}
+						data-type="currency"
+						placeholder="$0"
+						onChange={handleFromAmountChange}
+						onBlur={handleFromAmountBlur}
 					/>
 				</>
 			) : (

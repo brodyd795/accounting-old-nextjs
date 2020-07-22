63a0bcd43a1529c8d498ef9d56c75584d139ea8a
Brody Dingel <brodydingel795@gmail.com>
2020-07-22 07:14:54 -0500

    Move options into scope

--- pages/api/helpers/accounts-select-data-transform.js
+++ pages/api/helpers/accounts-select-data-transform.js
@@ -1,33 +1,33 @@
-const options = [
-    {label: "Income", options: []},
-    {label: "Expenses", options: []},
-    {label: "Assets", options: []},
-    {label: "Liabilities", options: []},
-    {label: "Virtual Savings", options: []}
-  ];
-
 const transformAccountsListForDropdown = (accounts) => {
-    accounts.map(account => {
-        let account_ui = account.slice(2);
-        switch (account[0]) {
-          case "I":
-            options[0].options.push({label: account_ui, value: account});
-            break;
-          case "E":
-            options[1].options.push({label: account_ui, value: account});
-            break;
-          case "A":
-            options[2].options.push({label: account_ui, value: account});
-            break;
-          case "L":
-            options[3].options.push({label: account_ui, value: account});
-            break;
-          case "V":
-            options[4].options.push({label: account_ui, value: account});
-            break;
-        }
-      });
-    return options;  
-}
+	const options = [
+		{ label: "Income", options: [] },
+		{ label: "Expenses", options: [] },
+		{ label: "Assets", options: [] },
+		{ label: "Liabilities", options: [] },
+		{ label: "Virtual Savings", options: [] },
+	];
+
+	accounts.map((account) => {
+		let account_ui = account.slice(2);
+		switch (account[0]) {
+			case "I":
+				options[0].options.push({ label: account_ui, value: account });
+				break;
+			case "E":
+				options[1].options.push({ label: account_ui, value: account });
+				break;
+			case "A":
+				options[2].options.push({ label: account_ui, value: account });
+				break;
+			case "L":
+				options[3].options.push({ label: account_ui, value: account });
+				break;
+			case "V":
+				options[4].options.push({ label: account_ui, value: account });
+				break;
+		}
+	});
+	return options;
+};
 
 export default transformAccountsListForDropdown;

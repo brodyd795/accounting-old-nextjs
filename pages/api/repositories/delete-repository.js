e02d2c2847e5d315f9ae37c78894cb61f137a3d1
Brody Dingel <brodydingel795@gmail.com>
2020-09-12 14:50:06 -0500

    WIP: Add transaction wrapper

--- pages/api/repositories/delete-repository.js
+++ pages/api/repositories/delete-repository.js
@@ -1,11 +1,9 @@
 import { createConn } from "./create-connection-repository";
+import { transactionWrapper } from "./transaction-wrapper-repository";
 
 export const deleteTransaction = async (row) => {
-	const conn = await createConn();
-
-	try {
-		await conn.query("START TRANSACTION");
-
+	const deleteIt = async () => {
+		const conn = await createConn();
 		const { trn_id, amount, from_account, to_account } = row;
 		await conn.query(`delete from transactions where trn_id = ?`, [trn_id]);
 		await conn.query(
@@ -16,13 +14,9 @@ export const deleteTransaction = async (row) => {
 			`UPDATE transactions SET from_balance = from_balance + ? WHERE trn_id > ? AND (from_account = ? OR from_account = ?)`,
 			[amount, trn_id, to_account, from_account]
 		);
-		await conn.query("COMMIT");
-		return "OK";
-	} catch (error) {
-		console.log("error", error);
-		await conn.query("ROLLBACK");
-		return "NOT OK";
-	} finally {
-		await conn.end();
-	}
+	};
+
+	const wrapped = transactionWrapper(deleteIt);
+	const result = await wrapped();
+	console.log("result", result);
 };

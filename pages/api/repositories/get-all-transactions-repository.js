0242c6a470e2434da951575246f6ec80f421e6a4
Brody Dingel <brodydingel795@gmail.com>
2020-09-28 22:10:24 -0500

    Migrate transactions page behind transactionWrapper

--- pages/api/repositories/get-all-transactions-repository.js
+++ pages/api/repositories/get-all-transactions-repository.js
@@ -1,24 +1,8 @@
-import { createConn } from "./create-connection-repository";
+import { withTransactionWrapper, conn } from "./transaction-wrapper-repository";
 
-export const getAll = async (isAdmin) => {
-	const conn = await createConn();
-
-	try {
-		if (isAdmin) {
-			await conn.query("START TRANSACTION");
-			let results = await conn.query(
-				`SELECT * FROM transactions ORDER BY trn_id desc`
-			);
-			await conn.query("COMMIT");
-			return results[0];
-		}
-
-		return { message: "error" };
-	} catch (error) {
-		console.log("error", error);
-		await conn.query("ROLLBACK");
-		return "NOT OK";
-	} finally {
-		await conn.end();
-	}
+const getAllAccountBalances = async (isAdmin) => {
+	return conn.query(`SELECT * FROM transactions ORDER BY trn_id desc`);
 };
+
+export default async (props) =>
+	withTransactionWrapper(getAllAccountBalances, props);
--- pages/api/services/recent-page-service.js
+++ pages/api/services/recent-page-service.js
@@ -1,10 +1,11 @@
 import { checkIsAdmin } from "./check-is-admin-service";
-import { getAll } from "../repositories/get-all-transactions-repository";
+import getAll from "../repositories/get-all-transactions-repository";
 
 export const getRecentPageData = async (user) => {
 	const isAdmin = checkIsAdmin(user);
 
 	const data = await getAll(isAdmin);
+
 	const dataWithoutEmail = data.map(({ user_email, ...rest }) => ({ ...rest }));
 
 	return dataWithoutEmail;

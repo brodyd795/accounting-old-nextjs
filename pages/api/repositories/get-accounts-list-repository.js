971ee1c92b29a63fb3d811e33408a2eef4af2b90
Brody Dingel <brodydingel795@gmail.com>
2020-09-28 22:01:32 -0500

    Get accounts page behind transactionWrapper

--- pages/api/repositories/get-accounts-list-repository.js
+++ pages/api/repositories/get-accounts-list-repository.js
@@ -1,23 +1,7 @@
-import { createConn } from "./create-connection-repository";
+import { withTransactionWrapper, conn } from "./transaction-wrapper-repository";
 
-export const getAccountsList = async () => {
-	const conn = await createConn();
-
-	try {
-		await conn.query("START TRANSACTION");
-
-		let accounts = await conn.query(
-			`SELECT acc_name FROM accounts WHERE open = true`
-		);
-		console.log("accounts", accounts);
-
-		await conn.query("COMMIT");
-		return accounts[0];
-	} catch (error) {
-		console.log("error", error);
-		await conn.query("ROLLBACK");
-		return "NOT OK";
-	} finally {
-		await conn.end();
-	}
+const getAllAccountBalances = async () => {
+	return conn.query(`SELECT acc_name FROM accounts WHERE open = true`);
 };
+
+export default async () => withTransactionWrapper(getAllAccountBalances);
--- pages/api/services/accounts-page-service.js
+++ pages/api/services/accounts-page-service.js
@@ -1,9 +1,10 @@
-import { getAccountsList } from "../repositories/get-accounts-list-repository";
+import getAccountsList from "../repositories/get-accounts-list-repository";
 import { transformAccountsListForDropdown } from "./accounts-select-data-transform";
 
 export const getAccountsPageData = async () => {
 	const data = await getAccountsList();
+
 	const accounts = data.map((account) => account.acc_name);
-	const transformedAccountsList = transformAccountsListForDropdown(accounts);
-	return transformedAccountsList;
+
+	return transformAccountsListForDropdown(accounts);
 };

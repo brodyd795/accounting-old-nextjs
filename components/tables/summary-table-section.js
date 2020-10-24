58bcb43820145d56fb23e0e8723a0a00fc08c893
Brody Dingel <brodydingel795@gmail.com>
2020-10-23 22:25:38 -0500

    Flip - signs to + on summary page

--- components/tables/summary-table-section.js
+++ components/tables/summary-table-section.js
@@ -4,11 +4,13 @@ import Link from 'next/link';
 import {toDollars} from '../../lib/currency-helpers';
 
 const SummaryTableSection = ({text, data}) => {
+	const shouldFlipSign = ['Liabilities', 'Income'].includes(text);
+
 	return (
 		<>
 			<tr>
 				<td>{text}</td>
-				<td className='balance'>{toDollars(data.balance)}</td>
+				<td className='balance'>{toDollars(data.balance, shouldFlipSign)}</td>
 			</tr>
 			{Object.entries(data.accounts).map(([account, accountValues]) => (
 				<Link
@@ -17,7 +19,9 @@ const SummaryTableSection = ({text, data}) => {
 					key={account}>
 					<tr className='account-row' key={account}>
 						<td className='account'>{accountValues.name}</td>
-						<td className='balance'>{toDollars(accountValues.balance)}</td>
+						<td className='balance'>
+							{toDollars(accountValues.balance, shouldFlipSign)}
+						</td>
 					</tr>
 				</Link>
 			))}

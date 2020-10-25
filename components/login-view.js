46ef9ab22787357c66c55e401ea6386b74e50d92
Brody Dingel <brodydingel795@gmail.com>
2020-10-24 20:51:09 -0500

    Add login view

--- components/login-view.js
+++ components/login-view.js
@@ -1,26 +1,39 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-const StyledLogin = styled.div`
-	display: flex;
-	width: 30%;
-	justify-content: center;
-	align-items: center;
+const StyledLoginWrapper = styled.div`
+	width: 20em;
 	height: 60px;
-	cursor: pointer;
+`;
+
+const StyledLoginButton = styled.button`
 	background-color: #222;
+	border: none;
+	width: 100%;
+	height: 100%;
+	color: white;
+	padding: 0;
+	cursor: pointer;
 	border: 1px solid black;
 	border-radius: 10px;
+	font-size: 20px;
+`;
+
+const StyledGuestMessageWrapper = styled.div`
+	display: flex;
+	justify-content: center;
 `;
 
 const LoginView = () => {
 	return (
-		<Link href='/api/auth/login'>
-			<>
-				<StyledLogin>{'Log in to continue'}</StyledLogin>
-				<p>{'(Guests will view demo data)'}</p>
-			</>
-		</Link>
+		<StyledLoginWrapper>
+			<Link href='/api/auth/login'>
+				<StyledLoginButton>Login</StyledLoginButton>
+			</Link>
+			<StyledGuestMessageWrapper>
+				<p>{'(Visitors will view demo data)'}</p>
+			</StyledGuestMessageWrapper>
+		</StyledLoginWrapper>
 	);
 };
 

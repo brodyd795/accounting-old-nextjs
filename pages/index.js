9ae241a16dd623e61b5c9c393445c19d9b7576d8
Brody Dingel <brodydingel795@gmail.com>
2020-10-27 20:14:39 -0500

    Add progress bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import useSWR from 'swr';
 import DatePicker from 'react-datepicker';
+import styled from 'styled-components';
 
 import fetch from '../lib/fetch';
 import {useFetchUser} from '../lib/user';
@@ -12,6 +13,37 @@ import Error from '../components/error';
 import {getMaxDate} from '../lib/date-helpers';
 import LoginView from '../components/login-view';
 
+const StyledFullBar = styled.div`
+	max-width: 500px;
+	background-color: black;
+	border-radius: 13px;
+	padding: 3px;
+	margin-bottom: 10px;
+`;
+
+const StyledFilledPart = styled.div`
+	background-color: ${props => props.myColor};
+	width: ${props => `${props.percent}%`};
+	height: 20px;
+	border-radius: 10px;
+`;
+
+const perc2color = (percent) => {
+	let r, g, b = 0;
+
+	if (percent < 50) {
+		r = 255;
+		g = Math.round(5.1 * percent);
+	} else {
+		g = 255;
+		r = Math.round(510 - 5.10 * percent);
+	}
+
+	let h = r * 0x10000 + g * 0x100 + b * 0x1;
+
+	return '#' + ('000000' + h.toString(16)).slice(-6);
+}
+
 const Index = () => {
 	const [selectedMonth, setSelectedMonth] = useState(new Date());
 	const {user, loading} = useFetchUser();
@@ -19,6 +51,8 @@ const Index = () => {
 		user ? `/api/controllers?user=${user.email}&date=${selectedMonth}` : null,
 		fetch
 	);
+	// const progress = data?.balances.expenses.balance / data?.lastMonthsIncome * -1 * 100;
+	const progress = 100;
 
 	if (error) return <Error />;
 
@@ -29,6 +63,9 @@ const Index = () => {
 			{user && (loading || !data) && <Loader />}
 			{user && data && (
 				<>
+					<StyledFullBar>
+						<StyledFilledPart percent={progress > 100 ? 100 : progress} myColor={perc2color(progress > 100 ? 100 : progress)}></StyledFilledPart>
+					</StyledFullBar>
 					<DatePicker
 						selected={selectedMonth}
 						onChange={date => setSelectedMonth(date)}
@@ -36,7 +73,6 @@ const Index = () => {
 						showMonthYearPicker
 						maxDate={getMaxDate()}
 					/>
-					<p>{`Last month's income: ${data.lastMonthsIncome}`}</p>
 					<SummaryTable data={data.balances} />
 				</>
 			)}
